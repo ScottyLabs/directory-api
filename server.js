33ed16444e2064e1bdcab1b64f09faf66a7c08b7
Oscar Bezi <oscar@bezi.io>
2015-11-05 15:13:41 -0500

    Fixed broken link.

--- server.js
+++ server.js
@@ -1,46 +1,135 @@
-var ldap = require('ldapjs')
-var express = require('express')
+/* @file server.js
+ * @brief The entire logic for the Directory API, which is just a thin shim
+ * around CMU LDAP.
+ *
+ * @author Shalom Yiblet (shalom@scottylabs)
+ * @author Oscar Bezi (bezi@scottylabs)
+ */
+'use strict';
+
+// =============================================================================
+// Config
+// =============================================================================
+/* @brief URL of the CMU LDAP server. */
+var ldapURL = 'ldap://ldap.andrew.cmu.edu';
+
+/* @brief The port to listen on. */
+var port = 8080;
+
+/* @brief Returned on 500 err codes. */
+var errString = 'Something went wrong.';
+
+// =============================================================================
+// LDAP
+// =============================================================================
+var ldap = require('ldapjs');
+
 var client = ldap.createClient({
-  url: 'ldap://ldap.andrew.cmu.edu'
-})
-// ldap.cmu.edu
+  url: ldapURL,
+});
 
+// =============================================================================
+//  Express
+// =============================================================================
+var express = require('express');
+var morgan = require('morgan');
 
-var app = express()
+var app = express();
+app.use(morgan('combined'));
 
+// =============================================================================
+// Some helper functions.
+// =============================================================================
 
-
+/* @brief Searches for an AndrewID using LDAP.
+ * @param String andrewID The AndrewID to look for.
+ * @param Function callback The callback.
+ */
 var search = function (andrewId, callback) {
   var options = {
     filter: `(cmuAndrewID=${andrewId})`,
-    scope: 'sub'
+    scope: 'sub',
   };
 
-  client.search('ou=person, dc=cmu, dc=edu', options, function (err, searchStream) {
-    if (err){ callback(err); return }
+  client.search('ou=person, dc=cmu, dc=edu', options, function (err, str) {
+    if (err) { callback(err); return; }
+
     var results = [];
 
-    searchStream.on('searchEntry', function (entry) {
-      results.push(entry.object)
-    })
-    searchStream.on('error', function (err) {
-      callback(err)
-    })
-    searchStream.on('end', function (result) {
-      callback(null, results)
-    })
-  })
-}
+    str.on('searchEntry', function (entry) {
+      results.push(entry.object);
+    });
 
-app.get('/andrewId/:id', function (req, res) {
+    str.on('error', function (e) {
+      callback(e);
+    });
+
+    str.on('end', function () {
+      callback(null, results);
+    });
+  });
+};
+
+/* @brief Error checking middleware that's the same for both endpoints.  */
+var errCheck = function (req, res, next) {
   search (req.params.id, function (err, response) {
     if (err) {
-      res.status(500)
-      res.end(JSON.stringify(err))
-      return
+      res.status(500);
+      res.end(errString);
+      return;
     }
-  res.end(JSON.stringify(response))
-  })
-})
 
-app.listen(8080)
+    if (response.length > 1) {
+      res.status(500);
+      res.end(errString);
+      return;
+    }
+
+    if (response.length === 0) {
+      res.status(404);
+      res.end('Person not found.');
+      return;
+    }
+
+    var raw = response[0];
+    req.rawData = raw;
+    next();
+  });
+};
+
+// =============================================================================
+// Routes.
+// =============================================================================
+app.get('/andrewId/:id/raw', errCheck, function (req, res) {
+  res.json(req.rawData);
+});
+
+app.get('/andrewId/:id', errCheck, function (req, res) {
+  var raw = req.rawData;
+
+  var person = {
+    // All requests will have these fields.
+    andrewID: raw.cmuAndrewId,
+
+    first_name: raw.givenName,
+    middle_name: raw.cmuMiddleName,
+    last_name: raw.sn,
+
+    preferred_email: raw.mail,
+
+    // This may or may not be in the query.
+    campus: raw.cmuCampus,
+    department: raw.cmuDepartment,
+    affiliation: raw.cmuPersonPrimaryAffiliation,
+    names: raw.cn,
+    job_title: raw.title,
+    office: raw.postalAddress,
+  };
+
+  res.json(person);
+});
+
+// Start up the server.
+app.listen(port,
+  () => console.log(`Directory API up and running on port ${ port }.`)
+);
