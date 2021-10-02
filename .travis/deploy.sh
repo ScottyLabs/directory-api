01de08afb3eeb8ff0392e2222ca801f804acbd96
etylu <etlu03@gmail.com>
2021-10-02 15:54:01 -0400

    Deleted useless files, made req.txt

--- .travis/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-git config --global push.default simple
-git remote add production ssh://git@chicago.club.cc.cmu.edu:/home/git/deploy/directory-api.git
-git push production master
